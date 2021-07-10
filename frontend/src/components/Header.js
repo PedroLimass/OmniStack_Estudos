d60d7b4c2b0e94818736680c7da51967b4869412
Pedro Lima <phlimas@outlook.com>
2021-07-09 21:59:24 -0300

    Set some files to learn frontend

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+    return (
+        <header id='main-header'>
+            <div className='header-content'>
+                <img src="" alt="Icone Marato" />   
+            </div>
+        </header>
+    )
+}
